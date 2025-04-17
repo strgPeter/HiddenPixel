6beb9c3ea46db4b85fc977be40340a81589107d8
strgPeter <peter2003lei@gmail.com>
2025-04-17 11:54:05 +0200

    fixed encoding error

--- src/main/kotlin/org/example/hiddenpixel/model/Encoder.kt
+++ src/main/kotlin/org/example/hiddenpixel/model/Encoder.kt
@@ -10,6 +10,7 @@ class Encoder {
         fun encode(msg: String, img: Image, channels: Map<Char, Boolean>): Image{
             val binMsg = msg.map {
                 it.code.toString(2).padStart(8, '0') }.joinToString("")
+            println("bin Msg: $binMsg")
             var binMsgPtr = 0
 
             val wdt = img.width.toInt()
@@ -25,26 +26,41 @@ class Encoder {
 
                     if (y==0 && x == 0){
                         encodeMsgLength(msg.length, pxlReader, pxlWriter)
+                        continue
                     } else if (y == 0 && x < 8) continue
 
                     val pxl = pxlReader.getArgb(x,y)
 
-                    if (binMsgPtr < binMsg.length){
+                    if (binMsgPtr < binMsg.length) {
                         // Extract channels:
                         val a = (pxl shr 24) and 0xFF
                         val r = (pxl shr 16) and 0xFF
                         val g = (pxl shr 8) and 0xFF
                         val b = pxl and 0xFF
 
+                        println(binMsgPtr)
+
                         var (newA, newR, newG, newB) = listOf(a, r, g, b)
-                        if (channels.getValue('a'))
-                            newA = (a and 0xFE) or ((binMsg[binMsgPtr++].digitToInt()) and 1)
-                        if (channels.getValue('r'))
-                            newR = (r and 0xFE) or ((binMsg[binMsgPtr++].digitToInt()) and 1)
-                        if (channels.getValue('g'))
-                            newG = (g and 0xFE) or ((binMsg[binMsgPtr++].digitToInt()) and 1)
-                        if (channels.getValue('b'))
-                            newB = (b and 0xFE) or ((binMsg[binMsgPtr++].digitToInt()) and 1)
+                        if (channels.getValue('a')) {
+                            newA = (a and 0xFE) or ((binMsg[binMsgPtr].digitToInt()) and 1)
+                            println("x: $x, y: $y, bit: ${binMsg[binMsgPtr]} binMsgPtr: $binMsgPtr")
+                            binMsgPtr++
+                        }
+                        if (channels.getValue('r')) {
+                            newR = (r and 0xFE) or ((binMsg[binMsgPtr].digitToInt()) and 1)
+                            println("x: $x, y: $y, bit: ${binMsg[binMsgPtr]} binMsgPtr: $binMsgPtr")
+                            binMsgPtr++
+                        }
+                        if (channels.getValue('g')) {
+                            newG = (g and 0xFE) or ((binMsg[binMsgPtr].digitToInt()) and 1)
+                            println("x: $x, y: $y, bit: ${binMsg[binMsgPtr]} binMsgPtr: $binMsgPtr")
+                            binMsgPtr++
+                        }
+                        if (channels.getValue('b')) {
+                            newB = (b and 0xFE) or ((binMsg[binMsgPtr].digitToInt()) and 1)
+                            println("x: $x, y: $y, bit: ${binMsg[binMsgPtr]} binMsgPtr: $binMsgPtr")
+                            binMsgPtr++
+                        }
 
                         // Reconstruct modified ARGB value
                         val newPxl = (newA shl 24) or (newR shl 16) or (newG shl 8) or newB
@@ -75,6 +91,7 @@ class Encoder {
                 val newPxl = (a shl 24) or (newR shl 16) or (g shl 8) or b
 
                 pw.setArgb(i, 0, newPxl)
+                println("x: $i, y: 0, pxl: $pxl, newPxl: $newPxl, bit: ${lengthBin[i]}")
             }
             println()
         }
